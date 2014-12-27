1bd5684a18df21c9af7184496259c3d3c0015e3b
kewos <gn00437805@gmail.com>
2014-12-27 17:01:22 +0800

    Add Dto

---
+++ MVCArchitecturePractice.Service.Dto/Dto/BaseDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCArchitecturePractice.Service.Dto
+{
+    public abstract class BaseDto
+    {
+        #region Properties
+        public Int64 ID { get; set; }
+        public DateTime AddDate { get; set; }
+        public DateTime? ModifyDate { get; set; }
+        #endregion
+    }
+}
---
+++ MVCArchitecturePractice.Service.Dto/Dto/MessageDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCArchitecturePractice.Service.Dto
+{
+    public class MessageDto : BaseDto
+    {
+        public string Comment { get; set; }
+        public Int64 UserId { get; set; }
+    }
+}
